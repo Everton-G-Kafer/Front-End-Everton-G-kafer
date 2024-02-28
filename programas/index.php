29993b4fe14228052dbb2a65ea5b3ad2e78144fe
Everton-G-Kafer <124881356+Everton-G-Kafer@users.noreply.github.com>
2024-02-28 12:23:48 -0300

    Add files via upload

---
+++ programas/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variavel</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <h1>Variaveis</h1>
+<body>
+    <?php
+    $nome = "𝔼𝕧𝕖𝕣𝕥𝕠𝕟";
+    const SOBRENOME = "𝕂𝕒𝕗𝕖𝕣";
+    echo "𝕄𝕖𝕦 𝕟𝕠𝕞𝕖 é $nome " .SOBRENOME; 
+    echo "<br>";
+
+    $idade = "𝟙𝟟";
+    echo "𝔼𝕦 𝕥𝕖𝕟𝕙𝕠 $idade 𝕒𝕟𝕠𝕤";
+    echo "<br>";
+
+    $peso = "𝟟𝟛";
+    echo "𝔼𝕦 𝕡𝕖𝕤𝕠 $peso 𝕢𝕦𝕚𝕝𝕠𝕤";
+    echo "<br>";
+
+    $altura = "𝟙,𝟠𝟝";
+    echo "𝔼𝕦 𝕞𝕖ç𝕠 $altura 𝕞𝕖𝕥𝕣𝕠𝕤";
+    ?>
+</body>
+</html>
